ebae8c09129c769bc9eaaa071f1e8c5fb465b160
Gabe Keeter <gabekeeter@gmail.com>
2021-02-14 23:17:50 -0600

    added Gamestate object and gave it a new room functionality. also added update method to NamedList to work with Gamestate

---
+++ Gamestate/gamestate.py
@@ -0,0 +1,15 @@
+from utils.named_list import NamedList
+from room_data import Rooms as rm
+
+
+class Gamestate(NamedList):
+    def __init__(self, player):
+        names = ['player', 'room', 'history']
+        vals = [player, rm.CC, []]
+        super(Gamestate, self).__init__(title="Gamestate", names=names, values=vals)
+
+    # Updates history by adding previous room and changes current room
+    def new_room(self, room):
+        history = self.get('history')
+        self.update('history', history.append(self.get('room')))
+        self.update('room', room)
